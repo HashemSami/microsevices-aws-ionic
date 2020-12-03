0006da685724062fed6585e96f4816d97c25fdcf
Hashem <hashem.sf@gmail.com>
2020-12-03 21:46:22 +0300

    fix url

--- udacity-c2-frontend/src/environments/environment.prod.ts
+++ udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "af5dbdbedb0704e30908297c364fb2b1-516266485.us-east-2.elb.amazonaws.com/api/v0"
+  apiHost: "http://af5dbdbedb0704e30908297c364fb2b1-516266485.us-east-2.elb.amazonaws.com/api/v0"
 };
 
 /*
