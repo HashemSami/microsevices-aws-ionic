84e9030bac6bd4d172b0d150f4ed97462640634a
Hashem <hashem.sf@gmail.com>
2020-12-03 18:18:54 +0300

    add proxy url to frontend

--- udacity-c2-frontend/src/environments/environment.prod.ts
+++ udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0"
+  apiHost: "af5dbdbedb0704e30908297c364fb2b1-516266485.us-east-2.elb.amazonaws.com/api/v0"
 };
 
 /*
--- udacity-c2-frontend/src/environments/environment.ts
+++ udacity-c2-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost: "af5dbdbedb0704e30908297c364fb2b1-516266485.us-east-2.elb.amazonaws.com/api/v0"
 };
 
 /*
